c1766ed2f537bf0393a334d2ea4e49eca88dab06
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-07-07 15:14:45 -0300

    Login Api

--- routes/api.php
+++ routes/api.php
@@ -60,6 +60,8 @@ Route::get('example/{mail}/{password}', function ($mail,$password,Request $reque
 //		"status" =>"nuevo"
 //	}
 */
+
+/*
 Route::get('login', function (Request $request) {
 	$jsonReq = json_decode($request->getContent(), true);//suponiendo que el body de la request es json
 
@@ -70,6 +72,50 @@ Route::get('login', function (Request $request) {
 
 	$user = App\User::where([
 		['email', '=', $jsonReq['email']],
+	])->take(1)->get();
+	
+	$client = \Laravel\Passport\Client::where([
+												['user_id', '=', $user[0]->id],
+												['password_client', '=', 1]
+											 ])->take(1)->get();
+	if(count($client) == 0){
+		$secret = str_random(40);
+		$client = new \Laravel\Passport\Client;
+		$client->user_id = $user[0]->id;
+		$client->name = $user[0]->name.'PasswordClient';
+		$client->secret = $secret;
+		$client->redirect = "http://localhost/";
+		$client->personal_access_client = false;
+		$client->password_client = true;
+		$client->revoked= false;
+		$client->save();
+		$respuesta = array(
+			"id" =>$client->id,
+			"secret" => $secret,
+			"status" =>"nuevo"
+		);
+	}else {
+		$respuesta = array(
+			"id" =>$client[0]['id'],
+			"secret" => $client[0]['secret'],
+			"status" =>"viejo"
+		);
+	}
+	return json_encode($respuesta);
+});*/
+
+
+
+Route::get('login/{mail}/{password}', function ($mail,$password) {
+	//$jsonReq = json_decode($request->getContent(), true);//suponiendo que el body de la request es json
+
+	if (!Auth::attempt(array('email' => $mail, 'password' => $password)))
+	{
+		return "Error: Credenciales Incorrectas";
+	}
+
+	$user = App\User::where([
+		['email', '=', $mail],
 	])->take(1)->get();
 	
 	$client = \Laravel\Passport\Client::where([
