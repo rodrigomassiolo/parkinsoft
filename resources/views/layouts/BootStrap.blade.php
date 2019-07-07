3d0daa16998a00b714184abc3b4ce0b76799f6b2
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-07-07 05:38:42 -0300

    bug Chart

--- resources/views/layouts/BootStrap.blade.php
+++ resources/views/layouts/BootStrap.blade.php
@@ -15,7 +15,7 @@
   <script src="{{ asset('js/jquery-slim.min.js') }}"></script>
   <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('js/feather.min.js') }}"></script>
-  <script src="{{ asset('js/chart.min.js') }}"></script>
+  <script src="{{ asset('js/Chart.min.js') }}"></script>
   <script src="{{ asset('js/dashboard.js') }}"></script>
 
 </body>
