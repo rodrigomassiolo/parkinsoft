15db52aa50bbcb2dfa8215bd6c1ca1d514420979
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-01 19:46:07 -0300

    test

--- scripts/csvtodb.sh
+++ scripts/csvtodb.sh
@@ -3,6 +3,6 @@ function csvtobd() {
 
 sudo mysql -u root -D 'parkinsoft' -e "LOAD DATA LOCAL INFILE '$1' INTO TABLE ENERGY FIELDS TERMINATED BY ';' LINES TERMINATED BY '\\n' IGNORE 1 ROWS (frameIndex, frameTime, pcm_LOGenergy) SET id = NULL, user_id = $2, created=NOW();";
 
-VALOR=$( csvtobd $1)
+VALOR=$( csvtobd $1 $2)
 echo "$VALOR"
 exit
