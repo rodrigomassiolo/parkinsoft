ea221e7c26b79583df123f830b6ef8573e91be84
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-01 20:39:22 -0300

    test

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -128,7 +128,7 @@ class AudioController extends Controller
     public function processEnergy(Request $request){//$user_id, $basepath){
         $user_id = 1; 
         $basepath = "/home/rodrigomassiolo/energy.csv";
-        exec("/var/www/html/parkinsoft/scripts/csvtodb.sh",$lineasLn);
+        exec("/var/www/html/parkinsoft/scripts/csvtodb.sh ".$basepath ." ".$user_id,$lineasLn);
         $resultado = '';
         foreach($lineasLn as $linea){
         $resultado = $resultado.$linea.'<br>';
