c65596d15cf40fd9be9deb290b3041e23429fa52
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-01 20:40:54 -0300

    test

--- scripts/csvtodb.sh
+++ scripts/csvtodb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo mysql -u root -D 'parkinsoft' -e "LOAD DATA LOCAL INFILE '/home/rodrigomassiolo/energy.csv' INTO TABLE ENERGY FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 ROWS (frameIndex, frameTime, pcm_LOGenergy) SET id = NULL, user_id = 1, created=NOW();";
+sudo mysql -u root -D 'parkinsoft' -e "LOAD DATA LOCAL INFILE '/home/rodrigomassiolo/energy.csv' INTO TABLE ENERGY FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 ROWS (frameIndex, frameTime, pcm_LOGenergy) SET id = NULL, user_id = 1, created=NOW();"
 exit
