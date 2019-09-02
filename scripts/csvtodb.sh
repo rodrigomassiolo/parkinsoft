6f08f6e12195506d98b1111b7e644e00e9a63fca
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-01 21:16:16 -0300

    pep

--- scripts/csvtodb.sh
+++ scripts/csvtodb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function csvenergy() {
 sudo mysql -u root -D 'parkinsoft' -e "LOAD DATA LOCAL INFILE '$1' INTO TABLE ENERGY FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 ROWS (frameIndex, frameTime, pcm_LOGenergy) SET id = NULL, user_id = $2, created=NOW();"
+
 }
 VALOR=$( csvenergy $1 $2)
 echo "$VALOR"
