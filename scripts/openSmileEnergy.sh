52ca0db36b2857a85cd957598e79cde8c4289958
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-02 20:02:15 -0300

    bugfix

--- scripts/openSmileEnergy.sh
+++ scripts/openSmileEnergy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 function openSmileEnergy() {
-cd /home/rodrigomassiolo/opensmile-2.3.0/bin/linux_x64_standalone_libstdc6
-sudo ./SMILExtract -C ../../config/demo/demo1_energy.conf -I  $1 -O $2 >/dev/null 2>&1
+(cd /home/rodrigomassiolo/opensmile-2.3.0/bin/linux_x64_standalone_libstdc6 && ./SMILExtract -C /home/rodrigomassiolo/opensmile-2.3.0/config/demo/demo1_energy.conf -I $1 -O $2)
 }
 VALOR=$( openSmileEnergy $1 $2)
 echo "$VALOR"
