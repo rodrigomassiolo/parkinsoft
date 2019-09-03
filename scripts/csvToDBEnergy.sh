7d98438727f17cca07971cb4d460c04262b30bb4
root <root@parkinsoft.us-central1-a.c.extended-line-246023.internal>
2019-09-03 01:29:48 +0000

    scriptsbash

--- scripts/csvToDBEnergy.sh
+++ scripts/csvToDBEnergy.sh
@@ -3,6 +3,6 @@ function csvToDBEnergy() {
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "LOAD DATA LOCAL INFILE '$1' INTO TABLE ENERGY FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 ROWS (frameIndex, frameTime, pcm_LOGenergy) SET id = NULL, user_id = $2, created=NOW();"
 
 }
-VALOR=$( csvToDBEnergy $1 $2)
+VALOR=$( csvToDBEnergy "$1" "$2" )
 echo "$VALOR"
 exit
--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function ffmpegf() {
-ffmpeg -i "$1" "$2"
+ffmpeg -i "$1" "$2" && chmod 777 "$2"
 }
 VALOR=$( ffmpegf "$1" "$2" )
 echo "$VALOR"
