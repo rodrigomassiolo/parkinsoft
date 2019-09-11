e18f3802b1ed9d50755652665a3d48f7264cf8a6
RodrigoMassiolo <rodrigomassiolo@gmail.com>
2019-09-10 23:34:54 -0300

    usuario actual fix

--- app/Http/Controllers/API/UsuarioController.php
+++ app/Http/Controllers/API/UsuarioController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
 use App\Rol;
+use App\Medico;
 use Illuminate\Support\Facades\Mail;
 class UsuarioController extends Controller
 {
