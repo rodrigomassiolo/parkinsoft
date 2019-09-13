df30bf730ca3bdd515ba7e8d8759362cb6030cb4
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-13 03:05:11 -0300

    fix 10 process audio

--- scripts/csvToDBProsodyAcf.sh
+++ scripts/csvToDBProsodyAcf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function csvToDBProsodyAcf() {
-mysql -u higia -p'Server123' -D 'parkinsoft' -e "LOAD DATA LOCAL INFILE '$1' INTO TABLE PROSODY FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 ROWS (name, frameTime, voiceProb_sma, F0_sma, pcm_loudness_sma) SET id = NULL, user_id = $2, ejerciciopaciente_id = $3, created=NOW();"
+mysql -u higia -p'Server123' -D 'parkinsoft' -e "LOAD DATA LOCAL INFILE '$1' INTO TABLE PROSODY FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 ROWS (frameTime, voiceProb_sma, F0_sma, pcm_loudness_sma) SET id = NULL, user_id = $2, ejerciciopaciente_id = $3, created=NOW();"
 
 }
 VALOR=$( csvToDBProsodyAcf "$1" "$2" "$3" )
