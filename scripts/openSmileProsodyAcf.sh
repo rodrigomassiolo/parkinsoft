acbf1293c9f9a334ea8539f4df8304213d1efad8
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-14 17:54:49 -0300

    prosody fix

--- scripts/openSmileProsodyAcf.sh
+++ scripts/openSmileProsodyAcf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function openSmileEnergy() {
-(cd /home/rodrigomassiolo/opensmile-2.3.0/bin/linux_x64_standalone_libstdc6 && ./SMILExtract -C /home/rodrigomassiolo/opensmile-2.3.0/config/demo/demo1_energy.conf -I "$1" -O "$2" && chmod 777 "$2")
+(cd /home/rodrigomassiolo/opensmile-2.3.0/bin/linux_x64_standalone_libstdc6 && ./SMILExtract -C /home/rodrigomassiolo/opensmile-2.3.0/config/prosodyAcf.conf -I "$1" -O "$2" && chmod 777 "$2")
 }
 VALOR=$( openSmileEnergy "$1" "$2" )
 echo "$VALOR"
