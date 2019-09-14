322b79df9d678e1dce030c1c3699475e73e3d691
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-14 03:41:17 -0300

    clear tables end fix

--- scripts/clearTables.sh
+++ scripts/clearTables.sh
@@ -5,8 +5,7 @@ mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from ENERGY where ejerci
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from AUDSPEC where ejerciciopaciente_id = $1"
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from PROSODY where ejerciciopaciente_id = $1"
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from EGEMAPS where ejerciciopaciente_id = $1"
-mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from pacienteEjercicio where id = $1"
-c="rm -f"; path="$2"; a=".*"; echo "$c" "$path""$a"; eval $c $path$a
+c="rm -f "; path="$2"; eval $c "$path" 
 }
 VALOR=$( clearTables "$1" "$2" )
 echo "$VALOR"
