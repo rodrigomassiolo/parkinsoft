0e14526fc96534f732c11ed92743490dbd7be0bb
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-13 23:58:07 -0300

    modify ClearTables

--- scripts/clearTables.sh
+++ scripts/clearTables.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 function clearTables() {
-mysql -u higia -p'Server123' -D 'parkinsoft' -e "truncate table CHROMA"
-mysql -u higia -p'Server123' -D 'parkinsoft' -e "truncate table ENERGY"
-mysql -u higia -p'Server123' -D 'parkinsoft' -e "truncate table AUDSPEC"
-mysql -u higia -p'Server123' -D 'parkinsoft' -e "truncate table PROSODY"
-mysql -u higia -p'Server123' -D 'parkinsoft' -e "truncate table EGEMAPS"
-
+mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from table CHROMA where ejerciciopaciente_id = '$1'"
+mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from table ENERGY where ejerciciopaciente_id = '$1'"
+mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from table AUDSPEC where ejerciciopaciente_id = '$1'"
+mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from table PROSODY where ejerciciopaciente_id = '$1'"
+mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from table EGEMAPS where ejerciciopaciente_id = '$1'"
+mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from table pacienteEjercicio where id = '$1'"
+rm -f "'$2'*"
 }
 VALOR=$( clearTables "$1" "$2" )
 echo "$VALOR"
