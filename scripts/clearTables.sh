359b4ca41d5be3f4f051941c4746f2c71db4ad11
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-14 01:23:59 -0300

    try fix clear tables

--- scripts/clearTables.sh
+++ scripts/clearTables.sh
@@ -6,6 +6,7 @@ mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from AUDSPEC where ejerc
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from PROSODY where ejerciciopaciente_id = $1"
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from EGEMAPS where ejerciciopaciente_id = $1"
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from pacienteEjercicio where id = $1"
+echo "$2*" 1>&3
 rm -f "$2*"
 }
 VALOR=$( clearTables "$1" "$2" )
