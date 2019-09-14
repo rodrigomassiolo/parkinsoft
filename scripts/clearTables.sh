9b7e4d2a686fe348ae1bd5c547cd4c8f15325863
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-09-14 02:11:09 -0300

    endfix ClearTables

--- scripts/clearTables.sh
+++ scripts/clearTables.sh
@@ -6,8 +6,7 @@ mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from AUDSPEC where ejerc
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from PROSODY where ejerciciopaciente_id = $1"
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from EGEMAPS where ejerciciopaciente_id = $1"
 mysql -u higia -p'Server123' -D 'parkinsoft' -e "delete from pacienteEjercicio where id = $1"
-echo "$2*" 1>&3
-rm -f "$2*"
+c="rm -f"; path="$2"; a=".*"; echo "$c" "$path""$a"; eval $c $path$a
 }
 VALOR=$( clearTables "$1" "$2" )
 echo "$VALOR"
