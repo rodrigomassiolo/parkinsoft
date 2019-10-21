b1da96b791244040069cbfb2e0ea9f8e129a7e58
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-10-20 23:28:36 -0300

    Correción Titulo Ejercicios

--- resources/views/abmEjercicio/index.blade.php
+++ resources/views/abmEjercicio/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>@lang('parkinsoft.audioListLink')</h2>
+                <h2>@lang('parkinsoft.abmExerciseLink')</h2>
             </div>
             <div class="float-right">
                 <a href="{{ route('abmEjercicio.create') }}" class="btn btn-success" >
