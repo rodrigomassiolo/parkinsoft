d4cf764c88bc03f9b4cf933094d44cd4a8356525
Rodrigo Massiolo <rodrigomassiolo@gmail.com>
2019-11-01 19:12:14 -0300

    language fix

--- resources/lang/en/parkinsoft.php
+++ resources/lang/en/parkinsoft.php
@@ -1,126 +1,294 @@
 <?php
-
 return [
-
-    /*
+ /*
     |--------------------------------------------------------------------------
     | Parkinsoft Language Lines
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used for the parkinsoft web app  English
+    | The following language lines are used for the parkinsoft web app English
     |
     */
-
     /*
     |--------------------------------------------------------------------------
     | General
     |--------------------------------------------------------------------------
     */
-
-    'accionDropdown' => 'Actions',
-    'ticketsLink' => 'Tickets',
-    'contactLink' => 'Contact',
-    'uploadAudio' => 'Upload audio',
-    'abmMedicLink' => 'Medic',
-    'abmUserLink' => 'Patients',
-    'abmAdminLink' => 'Admin',
-    'abmExerciseLink' => 'Exercise',
-    'audioListLink' => 'Exercises performed',
+    'abmMedicTitle' => 'Doctors',
+    'ticketsLinkTitle' => 'Tickets',
+    'abmUserLinkTitle' => 'Patients',
+    'abmAdminLinkTitle'      => 'Admin',
+    'abmExerciseLinkTitle' => 'Exercises',
+    'operacionLinkTitle' => 'Operations',
+    'accionDropdown'    => 'Actions',
+    'ticketsLink'       => 'Manage Tickets',
+    'contactLink'       => 'Contact us',
+    'uploadAudio'       => 'Upload audio',
+    'abmMedicLink'      => 'Manage Doctors',
+    'abmUserLink'       => 'Manage Patients',
+    'abmAdminLink'      => 'Manage Admin',
+    'abmExerciseLink'   => 'Manage Exercises',
+    'audioListLink'     => 'Exercises performed',
     'audioListLinkAssigned' => 'Exercises assigned',
     'downloadAppLink' => 'Download App',
-    'pleaseSelect' => 'Select an option',
-
+    'pleaseSelect' => 'Select option',
+    'operacionLink' => 'Manage Operations',
     'errorDescription' => 'There were some problems with your input',
-    'enableFilters' => 'Enable filters',
     'closeButton' => 'Close',
+    'acceptButton' => 'Accept',
+    'cancelButton' => 'Cancel',
     'deleteModalTitle' => 'Delete',
+    'modifyModalTitle' => 'Modify data',
+    'backButton' => 'Back',
+    'editButton' => 'Edit',
+    'createButton' => 'Create',
+    'deleteButton' => 'Delete',
+    'showButton' => 'Show',
+    'exampleButton' => 'Example',
+    'sendButton' => 'Send',
+    'showActionsButton' => 'Show actions',
+    'showHtmlGraphicButton' => 'Show graphic',
+    'downloadPdfGraphicButton' => 'Download as PDF',
+    'deleteAccount' => 'Delete Account',
+    'compareAudio' => 'Compare Audio',
+    'deleteAccountWarning' => 'Do you really want to delete your Parkinsoft account?',
+    'process' => 'Process',
+    'historial' => 'Show History',
+    'noHistorialToShow' => 'There are no audios uploaded!.',
+    'accountConfirmDelete' => 'Do you really want to delete your Parkinsoft account?',
+    /*
+    |--------------------------------------------------------------------------
+    | DataBase
+    |--------------------------------------------------------------------------
+    */
+     'BDLink'               => 'Manage Database',
+     'BDLinkTitle'          => 'Database',
+     'BDShowColumnsButton'  => 'Show Columns',
+     'BDConfirmDelete'      => 'Do you really want to delete the index?',
+     'BDColumnModalTitle'   => 'Columns',
+     'BDColumnName'         => 'Column Name',
+     'BDIndexName' => 'Index Name',
+     'BDDeleteIndexMessage' => 'Index deleted succesfully.',
+     'BDCreateNewButton' => 'Create Index',
+     'nameTable' => 'Table Name',
+     'BDCreateIndexMessage' => 'Index created succesfully.',
+     'BDModalTitle' => 'Index',
+    /*
+    |--------------------------------------------------------------------------
+    | Info
+    |--------------------------------------------------------------------------
+    */
+    'infoSiteTag' => 'About us',
+    'infoSiteBody1' => 'We are HigIA, made up of five colleagues from the computer engineering career of the University of La Matanza.',
+    'infoSiteBody2' => 'As an objective, we focus on developing informatics programs oriented to the health area. For this reason, we are interested in being able to provide solutions to various problems and innovate in new technologies.',
+    'infoSiteBody3' => 'We have the necessary knowledge to be able to face the challenges that are proposed to us. Therefore, we seek to remain at the forefront in the techniques and technologies that are applied today in the development of software and hardware.',
+    'infoSiteBody4' => 'Work in cooperation with our users to achieve the proposed objectives and coordinate with different areas of health to enrich our knowledge in order to offer alternatives in possible solutions to a given problem.',
     
-
+    'infoProjTag' => 'Project',
+    'infoProjBody1' => 'Parkinsoft is the first HigIA development. The goal is to create a program that allows the doctor to track the condition of the voice in Parkinsons patients.',
+    'infoProjBody2' => 'From an audio file (recording) of a patients voice, it is processed to obtain multiple variables to analyze. It is these results that will allow different types of analysis on the patients voice.',
+    
+    'infoProjList1Title' => 'The program has as input:',
+    'infoProjList1Bullet1' => 'Voice recordings of patients with the cell phone through an application and in controlled environments.',
+    'infoProjList1Bullet2' => 'Recordings during the Levodopa test on and off, before and after surgery.',
+    'infoProjList2Title' => 'It has as output:',
+    'infoProjList2Bullet1' => 'Generate different graphs and data, product of the processing of the audios, that will facilitate the doctor the possibility of making observations, analyzes and drawing conclusions about the current state of the patients voice at different times of the disease, in addition to being able to see the evolution in time.',
+    'infoProjList3Title' => 'Parkinsoft consists of two main parts:',
+    'infoProjList3Bullet1' => 'A mobile application (android) with which the patient can make recordings of his voice and send them to our server where the results obtained will be processed and saved. It should be noted at this point that the patient will not be able to visualize these results. In addition, the application will request to record the patient in periods that we define.',
+    'infoProjList3Bullet2' => 'A website where the doctor can observe the results of the analyzes performed on the patients audios. This website also covers the possibility of uploading the recordings that are generated outside the mobile application.',
+    'infoProjList4Title' => 'Advantages of our program, over others already used:',
+    'infoProjList4Bullet1' => 'Follow up over time.',
+    'infoProjList4Bullet2' => 'Versatility of analysis (we are not tied to a standard tool).',
+    'infoProjList4Bullet3' => 'Future possibility of adapting the tool to other diseases / pathologies.',
+    'infoProjList4Bullet4' => 'Free, this project is for us a way to give back to the university and the community the possibility of studying for free in a public university.',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Filters
+    |--------------------------------------------------------------------------
+    */
+    'enableFilters' => 'Enable filters',
+    'clearFilters' => 'Clear filters',
+    'filterFilters' => 'Filter',
+    'patient' => 'Patient',
+    'user' => 'User',
+    'description' => 'Description',
+    'date' => 'Date',
+    'dateFrom' => 'Date from',
+    'dateTo' => 'Date to',
+    'name' => 'Name',
+    'surname' => 'Surname',
+    'actions' => 'Actions',
+    'gender' => 'Gender',
+    'createDate' => 'Create Date',
+    'email' => 'Email',
+    'nacDate' => 'Birth Date',
+    'nacDateFrom' => 'Birth Date from',
+    'nacDateTo' => 'Birth Date to',
+    'dni' => 'Dni',
+    'password' => 'Password',
+    'passwordConfirm' => 'Confirm password',
     'language' => 'Language',
+    'medication' => 'Medication',
+    'matricula' => 'License',
+    'register' => 'Register',
+    'uploadAudio' => 'Upload audio',
+    'assingExercise' => 'Assign exercise',
+    'userDate' => 'My data',
+    'modifyData' => 'Edit data',
+    'selectGraphic' => 'Select graphic type',
+    'status' => 'Status',
+    'isLevodopa' => 'Levodopa',
+    'onOff' => 'On/Off',
+    'yes' => 'Yes',
+    'exercise' => 'Exercise',
+    'exerciseType' => 'Exercise type',
+    'emailDireccion' => 'Email',
+    'forgetPassword' => 'Forgot password?',
+    'noAccount' => 'Dont have an account?',
+    'registerHere' => 'Register here.',
+    'closeTicket' => 'Close ticket?',
+    'isLevodopaLabel' => '¿Is Levodopa?',
+    'uploadAudioLabel' => 'Upload Audio',
+    'uploadAudioTitle' => 'Upload Audio',
+    'audioQuality' => 'Audio quality',
+    'exerciseDescription' => 'Exercise description',
+  
+    'selectExerciseType' => 'Select Exercise',
+    'selectPatient' => 'Select Patient',
+    'selectAudioQuality' => 'Select audio quality',
+    'compareAudioFirst' => 'First audio to compare',
+    'compareAudioSecond' => 'Second audio to compare',
+    'oneConfiguration' => 'You must select at least one configuration',
+    'passwordFormat' => 'The password must have between 8 an 16 characters, including at least one lower case letter, one upper case letter and a number.',
     /*
     |--------------------------------------------------------------------------
     | Levodopa View
     |--------------------------------------------------------------------------
     */
-
     'levodopaTest' => 'Levodopa Test',
-    'languageTest' => 'THIS IS IN ENGLISH',
+    'languageTest' => '.',
     'levodopaFirstAudioText' => 'Upload audio without medication',
     'levodopaFirstAudioButton' => 'Upload',
     'levodopaSecondAudioText' => 'Upload audio with medication',
     'levodopaSecondAudioButton' => 'Upload',
     'levodopaModalTitle' => 'Upload audio',
-    'levodopaModalBody' => 'Your audio has been successfully uploaded!',
+    'levodopaModalBody' => 'Audio uploaded successfully!',
     'levodopaModalButton' => 'Close',
-
+    
     /*
     |--------------------------------------------------------------------------
     | Abm Users
     |--------------------------------------------------------------------------
     */
-
     'abmUserCreateTitle' => 'Create new patient',
-
+    'abmUserCreateNew' => 'Create patient',
+    'abmUserShowTitle' => 'Patient',
+    'abmUserEditTitle' => 'Edit patient',
     /*
     |--------------------------------------------------------------------------
     | Audio
     |--------------------------------------------------------------------------
     */
-
-    'audioUploadFileNotFound' => 'You must upload a file',
-    'audioInvalidExercise' => 'The exercise selected is invalid',
-    'audioNoMp3' => 'You can not upload an mp3',
-    'audioFileIsNotAudio' => 'You must upload a file that is an audio',
-    'audioInvalidFileUpload' => 'The file is invalid',
-    'audioFileTooBig' => 'The file is too big',
-    'audioUploadCorrectly' => 'File upload successfully',
-    'exerciseCompletedCorrectly' => 'Exercise Completed successfully',
-    
-
+    'audioUploadFileNotFound' => 'You must enter a file',
+    'audioInvalidExercise' => 'The requested exercise is invalid',
+    'audioNoMp3' => 'You must enter a file other than mp3 format',
+    'audioFileIsNotAudio' => 'You must enter an audio file',
+    'audioInvalidFileUpload' => 'The file entered is invalid',
+    'audioFileTooBig' => 'The file entered is too large',
+    'audioUploadCorrectly' => 'Audio loaded correctly',
+    'exerciseCompletedCorrectly' => 'Exercise done correctly',
     /*
     |--------------------------------------------------------------------------
     | Email
     |--------------------------------------------------------------------------
     */
-
     'emailResetPasswordButton' => 'Request new password',
-
-
-
-
     /*
     |--------------------------------------------------------------------------
     | Exercise
     |--------------------------------------------------------------------------
     */
-
-    'exerciseNewMessageSuccessful' => 'Ejercicio creado correctamente',
-    'exerciseUpdateMessageSuccessful' => 'Ejercicio modificado correctamente',
-    'exerciseDownloadExample' => 'Download Audio Example',
+    'exerciseModalTitle' => 'Exercise',
+    'exerciseNewMessageSuccessful' => 'Exercise created correctly',
+    'exerciseUpdateMessageSuccessful' => 'Exercise successfully modified',
+    'exerciseDeleteMessageSuccessful' => 'Exercise successfully deleted',
+    'exerciseConfirmDelete' => 'Do you really want to eliminate the exercise?',
+    'exerciseDuplicateMessageSuccessful' => 'The exercise you are trying to load already exists',
+    'exerciseCreateNew' => 'Create Exercise',
+    'exerciseDownloadExample' => 'Download sample audio',
     'downloadAudioPaciente' => 'Listen',
-
-        /*
+    'exerciseAudio' => 'Sample audio',
+    'exerciseEdit' => 'Edit exercise',
+    'exerciseDownload' => 'Download',
+    /*
     |--------------------------------------------------------------------------
-    | Ticket--> TRADUCIR
+    | Admin
     |--------------------------------------------------------------------------
     */
-
-    'ticketInsertComment' => 'Agregar comentario',
-    'ticketResponse'    => 'Comentar',
-    'ticketCloseButton' => 'Cerrar',
-    'ticketNewHelp' => 'Envianos tu consulta.',
-    'ticketNewContent' => 'Descripción',
-    'ticketNewTitle' => 'Titulo',
-    'ticketNewPageTitle' => 'Cargá tu consulta',
-    'ticketNewMessageSuccessful' => 'Su ticket ha sido creado.',
-    'ticketUpdateMessageSuccessful' => '¡El ticket ha sido actualizado!',
-    'ticketUpdateWithMailMessageSuccessful' => 'Se ha enviado un email al usuario con la respuesta',
-    'deleteMessageSuccessful' => 'El ticket ha sido borrado',
-    'title' => 'Titulo',
-    'response' => 'Respuesta',
-    'actualizar' => 'Actualizar',
-
-
+    'adminModalTitle' => 'Admin',
+    'adminNewMessageSuccessful'    => 'Admin created successfully',
+    'adminUpdateMessageSuccessful' => 'Admin successfully modified',
+    'adminDeleteMessageSuccessful' => 'Admin successfully removed',
+    'adminConfirmDelete' => 'Do you really want to delete Admin?',
+    'adminCreateNew' => 'Create Admin',
+    'adminEdit' => 'Edit Admin',
+    'adminDuplicateMessageSuccessful' => 'The admin you are trying to load already exists',
+    /*
+    |--------------------------------------------------------------------------
+    | User
+    |--------------------------------------------------------------------------
+    */
+    'userModalTitle' => 'Patient',
+    'userNewMessageSuccessful'    => 'Patient created successfully',
+    'userUpdateMessageSuccessful' => 'Patient successfully modified',
+    'userDeleteMessageSuccessful' => 'Patient successfully removed',
+    'userConfirmDelete' => 'Do you really want to eliminate the patient?',
+    /*
+    |--------------------------------------------------------------------------
+    | Medic
+    |--------------------------------------------------------------------------
+    */
+    'medicModalTitle' => 'Doctor',
+    'medicNewMessageSuccessful'    => 'Doctor created successfully',
+    'medicUpdateMessageSuccessful' => 'Doctor modified correctly',
+    'medicDeleteMessageSuccessful' => 'Doctor successfully removed',
+    'medicConfirmDelete' => 'Do you really want to eliminate the doctor?',
+    'medicCreateNew' => 'Create doctor',
+    'abmMedicEditTitle' => 'Edit doctor',
+    'medicShowTitle' => 'Doctor',
+    /*
+    |--------------------------------------------------------------------------
+    | Ticket
+    |--------------------------------------------------------------------------
+    */
+    'ticketInsertComment' => 'Add comment',
+    'ticketResponse'    => 'Comment',
+    'ticketCloseButton' => 'Close',
+    'ticketNewHelp' => 'Send us your question.',
+    'ticketNewContent' => 'Description',
+    'ticketNewTitle' => 'Title',
+    'ticketNewPageTitle' => 'Upload your question',
+    'ticketNewMessageSuccessful' => 'Your ticket has been created.',
+    'ticketUpdateMessageSuccessful' => 'The ticket has been updated!',
+    'ticketUpdateWithMailMessageSuccessful' => 'An email has been sent to the user with the response',
+    'deleteMessageSuccessful' => 'The ticket has been deleted',
+    'title' => 'Title',
+    'response' => 'Response',
+    'actualizar' => 'Update',
+    /*
+    |--------------------------------------------------------------------------
+    | Operation
+    |--------------------------------------------------------------------------
+    */
+    'operacionModalTitle' => 'Operation',
+    'operacionNewMessageSuccessful' => 'Operation created successfully',
+    'operacionUpdateMessageSuccessful' => 'Operation successfully modified',
+    'operacionDeleteMessageSuccessful' => 'Operation successfully removed',
+    'operacionConfirmDelete' => 'Do you really want to eliminate the operation?',
+    'operacionDuplicateMessageSuccessful' => 'The operation you are trying to load already exists',
+    'operacionCreateNew' => 'Create Operation',
+    'operacionEdit' => 'Edit Operation',
+    'operacionShowTitle' => 'Operation',
     /*
     |--------------------------------------------------------------------------
     | Anotador
